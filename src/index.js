8151867928fb92860dc8eed5ba6cf89a86c8044b
Loginov Vyacheslav <barthez.slavik@gmail.com>
2019-08-06 02:57:27 +0300

    Add m4a1

--- src/index.js
+++ src/index.js
@@ -37,6 +37,17 @@ var models = {
 		mtl: "models/Pirateship.mtl",
 		mesh: null
 	},
+	mac: {
+		obj: "models/Mac_2.obj",
+		mtl: "models/Mac_2.mtl",
+		mesh: null
+	},
+	ma: {
+		obj: "models/M4A1.obj",
+		mtl: "models/M4A1.mtl",
+		mesh: null,
+		castShadow: false
+	},
 	uzi: {
 		obj: "models/uziGold.obj",
 		mtl: "models/uziGold.mtl",
@@ -185,6 +196,25 @@ function init() {
 	scene.add(light);
 
         /*
+        var sphere = new THREE.SphereBufferGeometry(2, 32, 16);
+        var material1 = new THREE.MeshPhongMaterial({ color: 0xffaa00, flatShading: true, shininess: 0 });
+        var listener = new THREE.AudioListener();
+
+        var audioLoader = new THREE.AudioLoader();
+        var mesh1 = new THREE.Mesh(sphere, material1);
+        mesh1.position.set(0, 0, 0);
+        scene.add(mesh1);
+
+        var sound1 = new THREE.PositionalAudio(listener);
+        audioLoader.load('sounds/358232_j_s_song.ogg', function (buffer) {
+          sound1.setBuffer(buffer);
+          sound1.setRefDistance(20);
+          sound1.play();
+        });
+        mesh1.add(sound1);
+        */
+
+        /*
 	video = document.createElement( 'video' );
 	video.src = "videos/sintel.ogv";
 	video.load(); // must call after setting/changing source
@@ -280,6 +310,7 @@ function onResourcesLoaded() {
 	meshes["campfire1"] = models.campfire.mesh.clone();
 	meshes["campfire2"] = models.campfire.mesh.clone();
 	meshes["pirateship"] = models.pirateship.mesh.clone();
+	meshes["ma"] = models.ma.mesh.clone();
 
 	// Reposition individual meshes, then add meshes to scene
 	meshes["tent1"].position.set(-5, 0, 4);
@@ -297,6 +328,11 @@ function onResourcesLoaded() {
 	meshes["pirateship"].position.set(-11, -1, 1);
 	meshes["pirateship"].rotation.set(0, Math.PI, 0); // Rotate it to face the other way.
 	scene.add(meshes["pirateship"]);
+
+        meshes["ma"].position.set(10, 1, 0);
+	meshes["ma"].rotation.set(0, Math.PI, 0);
+	meshes["ma"].scale.set(0.12, 0.12, 0.12);
+	scene.add(meshes["ma"]);
 
 	// player weapon
 	meshes["playerweapon"] = models.uzi.mesh.clone();
